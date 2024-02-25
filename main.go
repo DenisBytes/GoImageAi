7d27c7ebb12afc6c57794b97e90629898e2378cc
DenisBytes <denisbytes@gmail.com>
2024-02-25 14:55:30 +0000

    [build]: project set up

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"embed"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+
+	"com.github.denisbytes.goimageai/handler"
+	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
+)
+
+//go:embed public/*
+var FS embed.FS
+
+func main() {
+	if err := initEnvVar(); err != nil {
+		log.Fatal("Init err: ", err)
+	}
+	router := chi.NewMux()
+
+	// Handler for static files
+	router.Handle("/*", http.StripPrefix("/", http.FileServer(http.FS(FS))))
+
+	router.Get("/", handler.MakeHandler(handler.HandleHomeIndex))
+
+	port := os.Getenv("HTTP_LISTEN_ADDR")
+	slog.Info("application running", "port", port)
+	log.Fatal(http.ListenAndServe(port, router))
+}
+
+func initEnvVar() error {
+	return godotenv.Load()
+}
